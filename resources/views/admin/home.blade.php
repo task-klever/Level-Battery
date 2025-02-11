deb38100718a32818201eec283326104cc6fad84
Alice <patelalicen@gmail.com>
2025-02-11 15:54:43 +0530

    basic cofig and hide extra component

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -37,7 +37,7 @@
                 </div>
             </div>
         </div>
-
+        <?php /* ?>
         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-success shadow h-100 py-2">
                 <div class="card-body">
@@ -53,7 +53,7 @@
                 </div>
             </div>
         </div>
-
+        
 
         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-success shadow h-100 py-2">
@@ -86,7 +86,7 @@
                 </div>
             </div>
         </div>
-
+<?php */ ?>
         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-success shadow h-100 py-2">
                 <div class="card-body">
